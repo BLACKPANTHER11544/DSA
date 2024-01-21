92dceefa7bf7c5dc291c283c740b247021e4715f
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-21 17:05:17 +0530

    Boilerplate_javaCode

---
+++ Java_code/Basic_of_java/Boilerplate_javaCode.java
@@ -0,0 +1,18 @@
+public class Boilerplate_javaCode{
+    public static void main(String args[]){
+        System.out.print(
+            "HELLO WORLD" 
+        );
+    }
+}
+
+// public class Boilerplate_javaCode => it is a public class and note the filename should be same as the class name . 
+// what are class => In object-oriented programming (OOP), a class is a blueprint or template for creating objects. Objects are instances of classes, and each object created from a class will have the same structure and behavior, as defined by the class. In simpler terms, a class defines the properties (attributes) and behaviors (methods) that objects of that class will have.
+// Here are the key components of a class:
+
+/*Attributes (Fields/Properties): These are the characteristics or data members of a class, representing the state of an object. Attributes are often defined using variables. For example, a Person class might have attributes like name, age, and address.
+
+Methods (Functions): These are the functions or procedures associated with a class, defining the behavior of the objects. Methods represent actions that objects of the class can perform. For example, a Car class might have methods like startEngine() and drive().
+
+Constructor: A special method called the constructor is used to initialize the attributes of an object when it is created. The constructor has the same name as the class and is called when an object is instantiated.*/
+// public static void main(String args[]): This line declares the main method, which serves as the entry point for the program. It takes an array of strings (args) as input.
