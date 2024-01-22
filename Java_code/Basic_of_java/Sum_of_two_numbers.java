1d0ce5348ecacd861e3d021c25a8e17d6bef6917
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-22 19:45:58 +0530

    sum of two numbers

---
+++ Java_code/Basic_of_java/Sum_of_two_numbers.java
@@ -0,0 +1,9 @@
+public class Sum_of_two_numbers{
+    public static void main(String args[]){
+        int sum ; 
+        final int number1 = 5 ; 
+        final int number2 = 10 ; 
+        sum = number1 + number2 ; 
+        System.out.print(sum);
+    }
+}
