ed6649bff4c840888ac264112b7a9c86033b18e0
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-24 16:27:10 +0530

    Type_casting

--- Java_code/Basic_of_java/Type_casting.java
+++ Java_code/Basic_of_java/Type_casting.java
@@ -4,7 +4,9 @@ public class Type_casting{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in); 
         System.out.println("Enter a decimal value"); 
+        // numb is of floating type variable
         float numb = sc.nextFloat(); 
+        // and now it(variable numb) has got type casted into interger data-type, by simply prefixing the data-type to which it want to get casted/converted to.
         int TypeCasted = (int) numb; 
         System.out.println("decimal value converted into intger value "+ TypeCasted);
     }
