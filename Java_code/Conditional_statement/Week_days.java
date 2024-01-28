873facca0c6036ca8ff2453df879835a473796ab
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-28 18:24:05 +0530

    Week Days

---
+++ Java_code/Conditional_statement/Week_days.java
@@ -0,0 +1,34 @@
+import java.util.Scanner ; 
+
+public class Week_days{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in); 
+        System.out.println("Enter any nnumber in between 1 to 7"); 
+        int weeknumb = sc.nextInt(); 
+        switch(weeknumb){
+            case 1 : 
+               System.out.println("Monday"); 
+               break ; 
+             case 2 : 
+               System.out.println("Tuesday"); 
+               break ;
+             case 3 : 
+               System.out.println("Webnesday"); 
+               break ;
+             case 4 : 
+               System.out.println("Thursday"); 
+               break ;
+             case 5 : 
+               System.out.println("Friday"); 
+               break ;
+             case 6 : 
+               System.out.println("Saturday"); 
+               break ;
+             case 7 : 
+               System.out.println("Sunday"); 
+               break ;
+            default : 
+               System.out.println("Can't Help");
+        }
+    }
+}
