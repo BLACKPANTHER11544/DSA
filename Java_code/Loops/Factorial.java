fca64489e47a9d3d40fa74dd17a2c8fe319cb991
BLACKPANTHER11544 <vaibhavgiri460@gmail.com>
2024-01-31 14:17:03 +0530

    multiplication table

--- Java_code/Loops/Factorial.java
+++ Java_code/Loops/Factorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner ; 
 
-class Factorial{  
+public class Factorial{  
  public static void main(String args[]){  
   Scanner sc = new Scanner(System.in); 
   System.out.println("Enter any number"); 
